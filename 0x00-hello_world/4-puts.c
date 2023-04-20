6f8ede6fce814f54aef560f47065d910a17e9b41
AnelisaMdawe <anelisamdawe494@gmail.com>
2023-04-20 10:20:21 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
 *main entry point
-* always return O (success)
+*Return: Always return O (success)
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
