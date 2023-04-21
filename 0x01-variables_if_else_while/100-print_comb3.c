526cd4b600c9ed6696b85000981449855e1431b3
AnelisaMdawe <anelisamdawe494@gmail.com>
2023-04-21 11:57:07 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,25 +6,17 @@
  */
 int main(void)
 {
-	int n, m, l;
+	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = 48; n <= 57; n++)
 	{
-		for (m = 49; m < 58; m++)
+		putchar(n);
+		if (n == 57)
 		{
-			for (l = 50; l < 58; l++)
-			{
-				if (l > m && m > n)
-				{putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
-					{
-						putchar(',');
-					}
-				}
-			}
+			continue;
 		}
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
