9583b72f5e170070811e78fa30475ffcd2dde338
AnelisaMdawe <anelisamdawe494@gmail.com>
2023-04-26 15:08:21 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
- * abs - computers the absolute value of an interger
+ * _abs - computers the absolute value of an interger
  * @n: the int to check
  * Return: the absolut value of in
  */
-int abs(int n)
+int _abs(int n)
 {
 	if (n >= 0)
 	{
 		return (n);
 	}
+	else
+
 	return (-n);
 }
