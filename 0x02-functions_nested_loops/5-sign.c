b155bee7b0f9639bc67f58412b373fdaa698a5a3
AnelisaMdawe <anelisamdawe494@gmail.com>
2023-04-26 15:00:09 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,11 +13,14 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-		_putchar(-1);
-		return ('-');
+		_putchar('0');
+		return (0);
 	}
-	_putchar(n);
-	return (n);
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
