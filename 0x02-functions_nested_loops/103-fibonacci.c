6e4b835d5b3c21ccffbfda8e0d4a16b3b090516c
AnelisaMdawe <anelisamdawe494@gmail.com>
2023-04-26 18:25:20 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,19 +9,20 @@ int main(void)
 	unsigned long  fib1 = 0, fib2 = 1, fibsum;
 	float tot_sum;
 
-		while (1)
-		{
-			fibsum = fib1 + fib2;
+	while (1)
+	{
+		fibsum = fib1 + fib2;
 
 		if (fibsum > 4000000)
-		       	break;
+
+			break;
+
 		if ((fibsum % 2) == 0)
 			tot_sum += fibsum;
 
 			fib1 = fib2;
-			fib2= fibsum;
-		}
-		printf("%lu\n",tot_ sum);
-
-		return (0);
+			fib2 = fibsum;
+	}
+	printf("%lu\n", tot_ sum);
+	return (0);
 }
