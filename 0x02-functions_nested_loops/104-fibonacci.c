7e4b1a93e6b739e63eb49c9f5fe4b206417f2eaf
AnelisaMdawe <anelisamdawe494@gmail.com>
2023-04-26 19:01:23 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -16,13 +16,12 @@ int main(void)
 	for (count = 0; count < 92; count++)
 	{
 		sum = fib1 = fib2;
-		printf(',' "%lu", sum);
+		printf(',',"%lu ", sum);
 
 		fib1 = fib2;
-		fib2 = sum;
+	fib2 = sum;
 	}
-
-	bef1_half1 = fib1 / 10000000000;
+	fib1_half1 = fib1 / 10000000000;
 	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
 	fib2_half2 = fib2 % 10000000000;
