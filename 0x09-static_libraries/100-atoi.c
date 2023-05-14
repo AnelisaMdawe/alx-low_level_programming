e234a6e40f869d2d3bbaddf21c53ea868addc085
AnelisaMdawe <anelisamdawe494@gmail.com>
2023-05-14 16:29:56 -0700

    0x09-static_libraries

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
+ *
  * Return: the int converted from the string
  */
 int _atoi(char *s)
@@ -18,22 +19,19 @@ int _atoi(char *s)
 
 	while (s[len] != '\0')
 		len++;
-
+	
 	while (i < len && f == 0)
 	{
-		if (s[i] == '-')
+			if (s[i] == '-')
 			++d;
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
-
 			if (d % 2)
-
-			digit = digit;
-			n = n * 10 + digit;
+				digit = -digit
+					n = n * 10 + digit;
 			f = 1;
-
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
@@ -45,28 +43,4 @@ int _atoi(char *s)
 		return (0);
 
 	return (n);
-}
-/**
- * main - The two numbers tha are multiplied
- * @argc: number of arguments
- * @argv: array arguments
- *
- * Return: 0 (Success), 1 (Error)
- */
-int main(int argc, char *argv[])
-{
-	int num1, num2, result;
-
-	if (argc < 3 || argc > 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 * num2;
-
-	printf("%d\n", result);
-
-	return (0);
 }
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - computers the absolute value of an interger
+ * @n: the int to check
+ * Return: the absolut value of in
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+
+	return (-n);
+}
