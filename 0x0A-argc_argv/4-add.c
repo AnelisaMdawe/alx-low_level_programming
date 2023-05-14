83341810e7e3b365022e37117db9ad698b8968b0
AnelisaMdawe <anelisamdawe494@gmail.com>
2023-05-14 08:36:04 -0700

    0xa.c-argc argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * check_num - checking if string is there are digit
+ * @str: array str
+ * Return: 0 (success)
+ */
+int check_num(char *str)
+{
+	unsigned count;
+
+	count = 0;
+
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - prints the name of the program
+ * @argc: count arguments find
+ * @argv: arrays arguments
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+
+	while (count < argc)
+	{
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
+		else
+		{
+				printf("error\n");
+				return (1);
+		}
+
+		count++;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
