df6f2f9eb717c66cbe47bcce5ee3360c3b09f80d
AnelisaMdawe <anelisamdawe494@gmail.com>
2023-05-15 19:19:52 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,7 +8,7 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *concat;
+	char *conct;
 	int i, ci;
 
 	if (s1 == NULL)
@@ -19,16 +19,16 @@ char *str_concat(char *s1, char *s2)
 		i++;
 	while (s2[ci] != '\0')
 		ci++;
-	concat = malloc(sizeof(char) * (i + ci + 1));
+	conct = malloc(sizeof(char) * (i + ci + 1));
 
-	if (concat == NULL)
+	if (conct == NULL)
 		return (NULL);
 	i = ci = 0;
 	while (s1[i] != '\0')
 	{
-		concat[i] = s1[i];
+		conct[i] = s1[i];
 		i++;
 	}
-	concat[i] = '\0';
-	return (concat);
+	conct[i] = '\0';
+	return (conct);
 }
