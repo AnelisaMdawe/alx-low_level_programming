678f82322a9930776e874b1baf5eaaa0c5a6b368
AnelisaMdawe <anelisamdawe494@gmail.com>
2023-05-16 13:50:29 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -36,16 +36,12 @@ char **strtow(char *str)
 
 	while (*(str + len))
 		len++;
-
 	words = count_word(str);
-
-		if (words == 0)
-			return (NULL);
-
+	if (words == 0)
+		return (NULL);
 	matrix = (char **) malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
 		return (NULL);
-
 	for (i = 0; i <= len; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
@@ -56,10 +52,8 @@ char **strtow(char *str)
 				tmp = (char *) malloc(sizeof(char) * (c + 1));
 				if (tmp == NULL)
 					return (NULL);
-
 				while (start < end)
 					*tmp++ = str[start++];
-
 				*tmp = '\0';
 				matrix[k] = tmp - c;
 				k++;
@@ -70,6 +64,5 @@ char **strtow(char *str)
 			start = i;
 	}
 	matrix[k] = NULL;
-
 	return (matrix);
 }
